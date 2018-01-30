5ff6bbc206991a577f4d9fff0c9a6194aaa886a0
Virginie Zinck <virginie.zinck@decathlon.com>
2018-01-30 11:56:36 +0100

    wip product 2

--- src/modules/product/Product.js
+++ src/modules/product/Product.js
@@ -4,8 +4,10 @@ import fetchProduct from '../../utils/product.services.js';
 class Product extends Component {
 
   componentDidMount() {
-    fetchProduct("2290a129-c530-43e5-8cec-984e4111aa7b").
-      then( product => console.log(product) );
+    fetchProduct("2290a129-c530-43e5-8cec-984e4111aa7b")
+      .then( product => console.log(product) )
+      .catch(error => console.warn(error));
+;
   }
 
   render() {
