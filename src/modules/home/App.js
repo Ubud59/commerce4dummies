17088ca5035bf251cd645adc424744a65bc295f4
Virginie Zinck <virginie.zinck@decathlon.com>
2018-01-30 11:22:56 +0100

    initial setup

--- src/modules/home/App.js
+++ src/modules/home/App.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Categories from '../categories/Categories';
+import Home from './Home';
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <AppBar position="static" color="default">
+            <Toolbar>
+              <Typography type="title" color="inherit">
+                Commerce4Dummies
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/categories" component={Categories}/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/modules/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Home (props) {
+  return (
+    <div>
+      <Link to="/categories">All sports</Link>
+    </div>
+  )
+}
+
+export default Home;
