1989c647b47c6f197129aeaa2b2609934e1bee31
Damien LEPRETRE <damien.lepretre@decathlon.com>
2018-01-31 17:40:56 +0100

    add tests on rating

--- src/modules/rating/Rating.js
+++ src/modules/rating/Rating.js
@@ -2,18 +2,14 @@ import React, { Component } from 'react';
 import Star from 'material-ui-icons/Star';
 import './Rating.css';
 
-class Rating extends Component {
-  render() {
-    console.log(this.props)
+function Rating(props) {
     return(
       <div>
         <div className="stars-outer">
-          <div className="stars-inner" style={{width: this.props.percent + "%"}}></div>
+          <div className="stars-inner" style={{width: props.percent + "%"}}></div>
         </div>
       </div>
     )
-  }
 }
-
 
 export default Rating
