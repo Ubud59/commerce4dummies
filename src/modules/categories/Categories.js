4172bf7cc6278fab7c6439721d51795525b1bf05
Damien LEPRETRE <damien.lepretre@decathlon.com>
2018-01-31 18:12:37 +0100

    fix style issue on card listing

--- src/modules/categories/Categories.js
+++ src/modules/categories/Categories.js
@@ -5,10 +5,11 @@ import { connect } from "react-redux";
 import { updateCategories } from "../../store/categories/actions";
 import { getCategoriesState } from "../../store/categories/selectors";
 
-import Card, { CardActions, CardContent } from 'material-ui/Card';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 import "./Categories.css"
+import categoryLogo from './category.jpg';
 
 class Categories extends Component {
 componentDidMount(){
@@ -24,8 +25,13 @@ render(){
     <div className="categoriesList">
       {this.props.categories.categories.map((category, index) =>
         <Card key={index} onClick={() => this.props.history.push(`/categories/${category.id}`)}>
-          <CardContent className="card-content">
-            <Typography type="headline" component="h2">
+          <CardMedia
+            className="media"
+            image={categoryLogo}
+            title="category"
+          />
+          <CardContent className="card-content-bis">
+            <Typography type="subheading" >
               {category.label}
             </Typography>
           </CardContent>
